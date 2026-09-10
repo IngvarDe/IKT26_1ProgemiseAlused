53fe5a38f30c6a62bbf1c46a8732efb72615276f
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-09-10 10:52:38 +0300

    ifAndElseNumbers

---
+++ 02 10.09.26/IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //konsoolis sisestame numbri
+            //see number salvestatakse string andmetüüpi
+            //toimub kontroll, kas sisestatud väärtus on number
+            //kasutada if ja else lauseid
+            string input = Console.ReadLine();
+
+            //kontrollime, kas sisestatud väärtus on number
+            if (int.TryParse(input, out int number))
+            {
+                Console.WriteLine($"Sisestatud number on: {number}");
+            }
+            else
+            {
+                Console.WriteLine("Sisestatud väärtus ei ole number.");
+            }
+        }
+    }
+}
