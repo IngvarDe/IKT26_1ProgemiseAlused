9451b9bb814b9cdb21543269300a8e8c4d2f4444
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-09-24 10:53:35 +0300

    IfElseMethodCall

---
+++ 04 24.09.26/IfElseMethodCall/IfElseMethodCall/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IfElseMethodCall
+{
+    internal class Program
+    {
+        //Main on meetod, mis läheb alati esimesena tööle
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //kasutada if ja else.
+            //kui kasutaja soovib, siis saab ta meetodi välja kutsuda
+            Console.WriteLine("Kui soovid meetotit välja kutsuda, siis kirjuta ja");
+            string method = Console.ReadLine();
+
+            if (method == "ja")
+            {
+                //kui kirjutan meetodi nime, siis seda nimetatakse
+                //meetodi välja kutsumiseks
+                Hellomethod();
+            }
+            else
+            {
+                Console.WriteLine("Ei soovinud midagi");
+            }
+        }
+
+        //teha teine meetod siia ja nimekson sellel HelloMethod
+        static void Hellomethod()
+        {
+            Console.WriteLine("Hello Kitty");
+        }
+    }
+}
